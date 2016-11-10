0aea7582362ceaaa10b31e71d864846f03e59601
BGTN <kaibgtn@web.de>
2016-11-10 11:19:47 -0500

    formatting

--- sense.js
+++ sense.js
@@ -11,110 +11,105 @@ var db = require('./config/mongo_database.js');
 console.log("Turn on the sensortag.");
 
 SensorTag.discover(function(sensorTag) {
-  console.log('discovered: ' + sensorTag);
+	console.log('discovered: ' + sensorTag);
 
-  sensorTag.on('disconnect', function() {
-    console.log('disconnected!');
-    process.exit(0);
-  });
+	sensorTag.on('disconnect', function() {
+		console.log('disconnected!');
+		process.exit(0);
+	});
 
-  async.series([
-      function(callback) {
-        console.log('connectAndSetUp');
-        sensorTag.connectAndSetUp(callback);
-      },
-      function(callback) {
-        console.log('readDeviceName');
-        sensorTag.readDeviceName(function(error, deviceName) {
-          console.log('\tdevice name = ' + deviceName);
-          callback();
-        });
-      },
-      function(callback) {
-        console.log('readSystemId');
-        sensorTag.readSystemId(function(error, systemId) {
-          console.log('\tsystem id = ' + systemId);
-          callback();
-        });
-      },
-      function(callback) {
-        console.log('readSerialNumber');
-        sensorTag.readSerialNumber(function(error, serialNumber) {
-          console.log('\tserial number = ' + serialNumber);
-          callback();
-        });
-      },
-      function(callback) {
-        console.log('enableAccelerometer');
-        sensorTag.enableAccelerometer(callback);
-      },
-      function(callback) {
-        setTimeout(callback, 2000);
-      },
-      function(callback) {
-        if (USE_READ) {
-          console.log('readAccelerometer');
-
-
-
-	setInterval(function(){
- 	 sensorTag.readAccelerometer(function(error, x, y, z) {
-		// save new accMeasurementEntry			
-		var accMeasurementEntry = new db.accMeasurementModel({
-			x: x,
-			y: y,
-			z: z
+	async.series([
+	function(callback) {
+		console.log('connectAndSetUp');
+		sensorTag.connectAndSetUp(callback);
+	},
+	function(callback) {
+		console.log('readDeviceName');
+		sensorTag.readDeviceName(function(error, deviceName) {
+			console.log('\tdevice name = ' + deviceName);
+			callback();
 		});
-		
-		accMeasurementEntry.save(function (err, accMeasurement) {
-			if (err) return console.error(err);
-			console.log("accMeasurement created");
-	
-			db.accMeasurementModel.find(function (err, accMeasurements) {
-				if (err) return console.error(err);
-				console.log(accMeasurements);
-			})				
+	},
+	function(callback) {
+		console.log('readSystemId');
+		sensorTag.readSystemId(function(error, systemId) {
+			console.log('\tsystem id = ' + systemId);
+			callback();
 		});
+	},
+	function(callback) {
+		console.log('readSerialNumber');
+		sensorTag.readSerialNumber(function(error, serialNumber) {
+			console.log('\tserial number = ' + serialNumber);
+			callback();
+		});
+	},
+	function(callback) {
+		console.log('enableAccelerometer');
+		sensorTag.enableAccelerometer(callback);
+	},
+	function(callback) {
+		setTimeout(callback, 2000);
+	},
+	function(callback) {
+		if (USE_READ) {
+			console.log('readAccelerometer');
 
-            console.log('\tx = %d G', x.toFixed(1));
-            console.log('\ty = %d G', y.toFixed(1));
-            console.log('\tz = %d G', z.toFixed(1));
+			setInterval(function() {
+				sensorTag.readAccelerometer(function(error, x, y, z) {
+					// save new accMeasurementEntry
+					var accMeasurementEntry = new db.accMeasurementModel({
+						x : x,
+						y : y,
+						z : z
+					});
 
-           // callback();
-          });
+					accMeasurementEntry.save(function(err, accMeasurement) {
+						if (err)
+							return console.error(err);
+						console.log("accMeasurement created");
 
-	}, 3000)
-	
+						db.accMeasurementModel.find(function(err, accMeasurements) {
+							if (err)
+								return console.error(err);
+							console.log(accMeasurements);
+						});
+					});
 
+					console.log('\tx = %d G', x.toFixed(1));
+					console.log('\ty = %d G', y.toFixed(1));
+					console.log('\tz = %d G', z.toFixed(1));
 
+					// callback();
+				});
 
-        } else {
-          sensorTag.on('accelerometerChange', function(x, y, z) {
-            console.log('\tx = %d G', x.toFixed(1));
-            console.log('\ty = %d G', y.toFixed(1));
-            console.log('\tz = %d G', z.toFixed(1));
-          });
+			}, 3000);
 
-          console.log('setAccelerometerPeriod');
-          sensorTag.setAccelerometerPeriod(500, function(error) {
-            console.log('notifyAccelerometer');
-            sensorTag.notifyAccelerometer(function(error) {
-              setTimeout(function() {
-                console.log('unnotifyAccelerometer');
-                sensorTag.unnotifyAccelerometer(callback);
-              }, 5000);
-            });
-          });
-        }
-      },
-      function(callback) {
-        console.log('disableAccelerometer');
-        sensorTag.disableAccelerometer(callback);
-      },
-      function(callback) {
-        console.log('disconnect');
-        sensorTag.disconnect(callback);
-      }
-    ]
-  );
+		} else {
+			sensorTag.on('accelerometerChange', function(x, y, z) {
+				console.log('\tx = %d G', x.toFixed(1));
+				console.log('\ty = %d G', y.toFixed(1));
+				console.log('\tz = %d G', z.toFixed(1));
+			});
+
+			console.log('setAccelerometerPeriod');
+			sensorTag.setAccelerometerPeriod(500, function(error) {
+				console.log('notifyAccelerometer');
+				sensorTag.notifyAccelerometer(function(error) {
+					setTimeout(function() {
+						console.log('unnotifyAccelerometer');
+						sensorTag.unnotifyAccelerometer(callback);
+					}, 5000);
+				});
+			});
+		}
+	},
+	function(callback) {
+		console.log('disableAccelerometer');
+		sensorTag.disableAccelerometer(callback);
+	},
+	function(callback) {
+		console.log('disconnect');
+		sensorTag.disconnect(callback);
+	}]);
 });
